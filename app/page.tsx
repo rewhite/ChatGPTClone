ce73e571bcc2d727148310c747f0a976ea5aea11
Hyun Bang <hsbang@uw.edu>
2023-05-24 17:21:13 -0700

    Fix iPhone 100vh issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
 import ChatPage from "@/components/ChatPage";
 import Image from "next/image";
 
 export default function Home() {
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+  window.addEventListener("resize", appHeight);
+  appHeight();
   return <ChatPage />;
 }
